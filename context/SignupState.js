b1122ea4b3772445b0ea843f10d330e69ba4a98b
Pablo Bacchetta <polbac@gmail.com>
2020-06-16 13:56:14 -0300

    1

--- context/SignupState.js
+++ context/SignupState.js
@@ -3,7 +3,7 @@ import Person from '../schema/Person'
 const SignupStateContext = createContext({})
 
 export function SignupStateContextProvider({children}) {
-    const [stepNumber, setStepNumber] = useState(2)
+    const [stepNumber, setStepNumber] = useState(1)
     const [petUUIDs, setPetUUIDs] = useState([])
     const [pets, setPets] = useState({})
     const [person, setPerson] = useState(new Person())
